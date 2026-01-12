ddb77b3afff3723aabc1df9f45c2231dadd656b5
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2026-01-12 16:09:41 -0700

    Add Redirect

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { navigate } from 'svelte-routing';
+	import { onMount } from 'svelte';
+
+	// Redirect without code so it doesn't cache
+	onMount(() => {
+		navigate('/chat');
+	});
+</script>
